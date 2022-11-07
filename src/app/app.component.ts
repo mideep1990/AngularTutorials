9374589fe36e367e206917953062998ec61635e7
mideep1990 <mideepankar@gmail.com>
2022-11-07 18:49:56 +0530

    interpolation code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-course';
+
+  data = {
+    content1:"First Content",
+    content2:"Second Content",
+    content3:"Third Content"
+  }
+  firstDivContent : string = "<script>alert('dhdf')</script>I have some apples.First Div Content. Lorem ...."
+  secondDivContent : string = "Second Div Content. Lorem 2...."
+  thirdDivContent : string = "Third Div Content. Lorem 3...."
+
+  keyUpFunction(updatedValue:string)
+  {
+    this.data.content1 = updatedValue
+  }
 }
+
